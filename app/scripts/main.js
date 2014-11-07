62d0d589c27a9f457e0131842bb268a7e7138cc1
Benjamin Smith <benjamin.smith@72andsunny.com>
2014-11-07 15:50:22 -0800

    added replay

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,12 +2,26 @@
 	$('#cookie-video').tubeplayer({
 	  initialVideo: '7HrnWC8zBcE',
 	  showinfo: false,
-	  showControls: false
+	  showControls: false,
+	  'onPlayerEnded':checkAgain
 	});
+
+	function checkAgain(){
+		$('#cookie-video').animate({'opacity':'.5'});
+		$('.try-again').fadeIn("slow");
+	}
 
 	$('#cookie-button').on('click',function(){
 		$('.start').fadeOut("slow");
-		$('#cookie-video').fadeIn();
+		$('#cookie-video').fadeIn("slow");
 		$('#cookie-video').tubeplayer('play');
 	});
+
+	$('.again').on('click',function(){
+		$('.try-again, #cookie-video').hide();
+		$('#cookie-video').animate({'opacity':'1'});
+		$('.start').show();
+	});
+
+
 })();
